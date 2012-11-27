fa2c1648ea64e3832fa7dfa8710c497721e13878
Hans Höglund <hans@hanshoglund.se>
2012-11-27 15:00:30 +0100

    - Tentative loader impl

---
+++ src/Language/Modulo/Loader.hs
@@ -0,0 +1,17 @@
+
+{-# LANGUAGE DisambiguateRecordFields, TypeFamilies,
+    StandaloneDeriving, DeriveFunctor, DeriveFoldable, GeneralizedNewtypeDeriving #-}
+
+-------------------------------------------------------------------------------------
+-- |
+-- Copyright   : (c) Hans Hoglund 2012
+-- License     : BSD-style
+-- Maintainer  : hans@hanshoglund.se
+-- Stability   : experimental
+-- Portability : GHC
+--
+--
+-------------------------------------------------------------------------------------
+
+module Language.Modulo.Loader ( 
+  ) where
